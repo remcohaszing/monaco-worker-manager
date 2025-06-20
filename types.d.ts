4e38f5fc2d6a3acac0cdda613eac39300b6754f0
Remco Haszing <remcohaszing@gmail.com>
2025-06-20 09:09:44 +0200

    Use TypeScript outDir

--- types.d.ts
+++ types.d.ts
@@ -1,5 +1,5 @@
 declare module 'monaco-editor/esm/vs/editor/editor.worker.js' {
-  import { type worker } from 'monaco-editor/esm/vs/editor/editor.api'
+  import { type worker } from 'monaco-editor/esm/vs/editor/editor.api.js'
 
   export function initialize<C>(fn: (ctx: worker.IWorkerContext, createData: C) => unknown): unknown
 }
